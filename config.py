5e1916b4138660d22d3d01a3150d0b753339e6d1
Gabriel41628 <gabriel41628@gmail.com>
2025-04-01 22:21:29 -0400

    fix(config): retira credenciais hardcoded

--- config.py
+++ config.py
@@ -1,9 +1,6 @@
 import os
-from dotenv import load_dotenv
 
-load_dotenv()
-
-BOT_TOKEN = os.getenv("7957239746:AAGr78KIPdWBK1-QajD35NtjsHjk_P0UpD4")
-SMM_API_KEY = os.getenv("07d335bc41e9567ec1d3c6a0763e2554")
-SMM_API_URL = os.getenv("https://upmidiass.net/api/v2")
-MERCADO_PAGO_TOKEN = os.getenv("APP_USR-8017733387483580-040117-b163d6c7fcdfb00930f24df53b8501ad-2040475905")
+BOT_TOKEN = os.environ["BOT_TOKEN"]
+SMM_API_KEY = os.environ["SMM_API_KEY"]
+SMM_API_URL = os.environ["SMM_API_URL"]
+MERCADO_PAGO_TOKEN = os.environ["MERCADO_PAGO_TOKEN"]
